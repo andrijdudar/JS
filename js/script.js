0f7b94f691ec4c2b662a4ef06b6dcffbc406c5f5
andrijdudar <andrij.dudar@gmail.com>
2022-05-13 17:10:43 +0300

    adc

--- js/script.js
+++ js/script.js
@@ -1,52 +1,52 @@
-//let jss = prompt(`Яка “офиційна” назва JavaScript?`, 'ECMAScript');
-//if (jss == "ECMAScript") {
-//	alert("Правильно");
+let jss = prompt(`Яка “офиційна” назва JavaScript?`, 'ECMAScript');
+if (jss == "ECMAScript") {
+	alert("Правильно");
 
-//} else {
-//	alert(`Не знаєте? “ECMAScript”!`);
-//};
+} else {
+	alert(`Не знаєте? “ECMAScript”!`);
+};
 
-//let jsss = prompt(`Яка “офиційна” назва JavaScript?`, 'ECMAScript');
-//(jsss == `ECMAScript`)
-//	? alert("Правильно!")
-//	: alert("Неправильно.");
+let jsss = prompt(`Яка “офиційна” назва JavaScript?`, 'ECMAScript');
+(jsss == `ECMAScript`)
+	? alert("Правильно!")
+	: alert("Неправильно.");
 
-//let namber = prompt("Число");
-//if (namber > 0) {
-//	alert("1");
-//} else if (namber < 0) {
-//	alert("-1");
-//} else if (namber == 0) {
-//	alert("0");
-//} else {
-//	alert("Це не число");
-//};
+let namber = prompt("Число");
+if (namber > 0) {
+	alert("1");
+} else if (namber < 0) {
+	alert("-1");
+} else if (namber == 0) {
+	alert("0");
+} else {
+	alert("Це не число");
+};
 
-//let a = 1;
-//let b = 3;
-//let result = (a + b < 4) ? "Нижче" : "Вище";
-//alert(result);
+let a = 1;
+let b = 3;
+let result = (a + b < 4) ? "Нижче" : "Вище";
+alert(result);
 
-//let login = prompt("Посада", '')
-//let massage = (login == "Працівник") ? 'Привіт' :
-//	(login == 'Директор') ? 'Вітаю' :
-//		(login == '') ? 'Немає логіну' :
-//			'-';
-//alert(massage);
+let login = prompt("Посада", '')
+let massage = (login == "Працівник") ? 'Привіт' :
+	(login == 'Директор') ? 'Вітаю' :
+		(login == '') ? 'Немає логіну' :
+			'-';
+alert(massage);
 
-//let hour = 12;
-//let isWeekend = true;
-//if (hour < 10 || hour > 18 || isWeekend) {
-//	alert('Офіс зачинений.');
-//}
+let hour = 12;
+let isWeekend = true;
+if (hour < 10 || hour > 18 || isWeekend) {
+	alert('Офіс зачинений.');
+}
 
-//let firstName = '';
-//let lastName = '';
-//let nickName = '';
-//alert(firstName || lastName || nickName || 'Anonim');
+let firstName = '';
+let lastName = '';
+let nickName = '';
+alert(firstName || lastName || nickName || 'Anonim');
 
-//let hour = 12;
-//let minute = 30;
-//if (hour == 12 && minute == 30) {
-//	alert('Час 12:30');
-//}
+let hour = 12;
+let minute = 30;
+if (hour == 12 && minute == 30) {
+	alert('Час 12:30');
+}
