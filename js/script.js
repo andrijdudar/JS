305741746082a51149b73738e02f2aea8aea83e7
andrijdudar <andrij.dudar@gmail.com>
2022-07-07 17:14:29 +0300

    s

--- js/script.js
+++ js/script.js
@@ -1,42 +1,6 @@
-'use strict'
-/*
-let user = {
-	name: `Іван`,
-	age: 30,
-};
-*/
-
-//let user = {
-//	name: `Іван`,
-//	age: 30,
-//};
-//user.isAdmin = true;
-//user[`likes birds`] = `test1`;
-
-//let user = {};
-//user[`любить птахів`] = true;
-//alert(user[`любить птахів`]);
-////delete user[`любить птахів`];
-////alert(user[`любить птахів`]);
-
-//let key = `квас`;
-//user[key] = `Тарас`;
-//alert(user[key]);
-//console.log(user);
-
-
-//?let user = {
-//?	name: `Іван`,
-//?	age: 30,
-//?};
-//?let key = prompt(`що ви хочете знати про користувача?`, 'name');
-//?alert(user[key]);
-
-let fruit = prompt('Які фрукти купити?', 'apple');
-
-let vegitabels = prompt('Які фрукти купити?', 'cucumber');
-lat bag = {
-	[fruit]: 5,
-	[vegitabels]: 7
-};
-ob
+let fruit = [`Apple`, `Orange`, `Plum`];
+document.write(fruit[1]);
+fruit[1] = `Pear`;
+document.write(fruit[1]);
+console.log(fruit[4]);
+dw
